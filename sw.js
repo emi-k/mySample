0c88d5bfe37fa771f0e6286bd896b26a199ad981
emi-k <45205638+emi-k@users.noreply.github.com>
2022-01-24 13:45:43 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// PWA対応：Service Worker
+
+var CACHE_NAME = 'sample-app';
+var urlsToCache = [
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
